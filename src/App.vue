74956a650619aa95a8d36d05d09d595f447b94ee
Mittelbach <iris.mittelbach@paessler.com>
2022-10-19 10:07:03 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,13 @@
 </template>
 
 <script setup>
+import {onMounted, ref} from "vue";
+const apiData = ref(null)
+async function getMemes() {
+  await fetch('https://api.imgflip.com/get_memes')
+  // get them from https://api.imgflip.com/get_memes data => memes
+  // transform json into array of meme objects (with id, url, name)
+}
 </script>
 
 <style lang="scss">
