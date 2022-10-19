4e1819ca0ea1a83a1cbc31a846b218be0e44f15d
Matthias Gohla <matthias.gohla@paessler.com>
2022-10-19 11:21:34 +0200

    mob next [ci-skip] [ci skip] [skip ci]

--- src/useMemeComparer.js
+++ src/useMemeComparer.js
@@ -2,12 +2,22 @@ import {ref, unref} from "vue";
 
 export const useMemeComparer = (memeList) => {
     const randomPair = ref([])
-    function getRandom() {
+    
+    function getRandom(idToAvoid) {
         const memeArray = unref(memeList)
+        const indexToAvoid = memeArray.findIndex(({id} ) => id === idToAvoid)
+        if(indexToAvoid > -1 ) {
+            memeArray.splice(indexToAvoid, 1)
+        }
         return memeArray[Math.floor(Math.random()*memeArray.length)];
     }
 
+    function getRandomPair() {
+        randomPair.value = [getRandom(), getRandom(memeOne.id)]
+    }
+
     return {
-        randomPair
+        randomPair,
+        getRandomPair
     }
 }
