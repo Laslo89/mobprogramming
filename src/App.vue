d4e619156bef9b01ec717c714a89bba76ca4e13a
Matthias Gohla <matthias.gohla@paessler.com>
2022-10-19 10:19:49 +0200

    mob next [ci-skip] [ci skip] [skip ci]

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <h1> Meme Rating App </h1>
+
 </template>
 
 <script setup>
 import {onMounted, ref} from "vue";
-const apiData = ref(null)
+const memes = ref(null)
+
 async function getMemes() {
-  await fetch('https://api.imgflip.com/get_memes')
+  try {
+    const jsonResult = await fetch('https://api.imgflip.com/get_memes')
+    const result = await jsonResult.json()
+    memes.value = result.data.memes.map(({id, name, url}) => ({ id, name, url }))
+    console.log(memes.value);
+    localStorage.setItem('memes', JSON.stringify(memes.value))
+  } catch (error) {
+    console.log(error);
+  }
   // get them from https://api.imgflip.com/get_memes data => memes
   // transform json into array of meme objects (with id, url, name)
 }
+getMemes()
 </script>
 
 <style lang="scss">
